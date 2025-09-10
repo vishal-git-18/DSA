1ead8f713d3c0883713d4fb48983b5aece81fab4
vishal-git-18 <vishalravishankar18@gmail.com>
2025-09-10 21:38:49 +0530

    Floyd's Cycle Detection

---
+++ Floyds-Cycle-Detection.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        // Phase 1: Find intersection point of two runners
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance to the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# DSA" 
+# 📘 DSA Reference Repository
+
+This repository contains a collection of **Data Structures & Algorithms (DSA)** problems and solutions that I found useful during my learning and practice.  
+The goal is to provide a reference for others who want to quickly look up implementations or understand common patterns.
